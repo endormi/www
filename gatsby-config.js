b9da35baee32e164dd6559eefc047b53bd62dbd8
endormi <ernosalo14@gmail.com>
2019-07-20 17:07:24 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,18 +30,6 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'standalone',
-        icons: [
-          {
-            src: '/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/favicons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
       },
     },
     /* Must be placed at the end */
